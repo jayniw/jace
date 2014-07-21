b6b4de5c0a35154d051d5d5826ac5a137bba6ecd
jayniw <jayniw@gmail.com>
2014-07-21 15:08:45 -0400

    primer commit

---
+++ src/app.php
@@ -0,0 +1,46 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+
+$app = new Application();
+$app->register(new UrlGeneratorServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new TwigServiceProvider());
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+    // add custom globals, filters, tags, ...
+
+    return $twig;
+}));
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options'    => array(
+      'driver'        => 'pdo_odb',
+      'host'          => 'localhost',
+      'dbname'        => 'jace',
+      'user'          => 'jace',
+      'password'      => 'jace',
+      'charset'       => 'utf8',
+      'driverOptions' => array(1002 => 'SET NAMES utf8',),
+    ),
+  ));
+   
+  /*$app->register(new Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider, array(
+      "orm.em.options" => array(
+           "mappings" => array(
+              array(
+                 "type"      => "yml",
+                 "namespace" => "Entity",
+                 "path"      => realpath(__DIR__."/../config/doctrine"),
+                ),
+              ),
+           ),
+  ));*/
+
+
+
+return $app;
